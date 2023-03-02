9370c61516a32c62c3f24e2b93ea56c6ca03168f
suelebesenzoni <suele.besenzoni@gmail.com>
2023-03-02 12:17:13 +0100

    test 2

--- Assets/Scripts/ciao.cs
+++ Assets/Scripts/ciao.cs
@@ -7,7 +7,7 @@ public class ciao : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("sera");
     }
 
     // Update is called once per frame
