0a228f3e883a1584b6a1bab3ae29a10e133294ce
St3fan044 <ottobreste@gmail.com>
2023-03-02 12:17:43 +0100

    test 3

--- Assets/Scripts/ciao.cs
+++ Assets/Scripts/ciao.cs
@@ -7,7 +7,7 @@ public class ciao : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log ("ciao");
     }
 
     // Update is called once per frame
