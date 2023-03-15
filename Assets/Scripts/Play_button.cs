fa67d31e3397b410890a5105f068f2936f968bf3
suelebesenzoni <suele.besenzoni@gmail.com>
2023-03-15 15:24:47 +0100

    Animazioni & cambio scena

--- Assets/Scripts/Play_button.cs
+++ Assets/Scripts/Play_button.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
- public class Play_button_bg : MonoBehaviour
- {
-    public void play()
+public class Play_button : MonoBehaviour
+{
+    public void MoveToScene(int sceneID)
     {
-        SceneManager.LoadScene("3.1_Level_Game_BG 1");
+        SceneManager.LoadScene(sceneID);
     }
 }
