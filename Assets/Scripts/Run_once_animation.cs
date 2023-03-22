2de09a25532917ff97064f31685ff90b6d465535
St3fan044 <ottobreste@gmail.com>
2023-03-22 10:02:38 +0100

    Ste Modifiche 8
    Animazioni

---
+++ Assets/Scripts/Run_once_animation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Run_once_animation : MonoBehaviour
+{
+    GameObject animazione;
+
+    void Start()
+    {
+       // Animation["Logo_Animation"].wrapMode = WrapMode.Once;
+       // Animation.Play("Logo_Animation");
+    }
+
+    void Update()
+    {
+    }
+}
+    
