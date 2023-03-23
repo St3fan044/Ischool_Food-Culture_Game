db08507455edc3e05a2f0c735a2c204bb80379a4
suelebesenzoni <suele.besenzoni@gmail.com>
2023-03-23 15:13:15 +0100

    Start buttons
    Da aggiungere collegamento scene

---
+++ Assets/Scripts/2_Start_home/LanguageButton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ButtonType
+{
+    LANGUAGE,
+    GAMEMODE
+}
+
+public class LanguageButton : MonoBehaviour
+{
+    public bool selected;
+    public LanguageButton otherLanguage;
+    public Image sprite;
+    public Color selectedColor;
+
+    public ButtonType buttonType;
+
+    public void ToggleLanguage()
+    {
+        if (selected == true) return;
+        selected = true;
+
+        if (selected == true)
+        {
+            otherLanguage.selected = false;
+        }
+    }
+
+    private void Update()
+    {
+        sprite.color = selected == true ? Color.white : selectedColor;
+    }
+}
